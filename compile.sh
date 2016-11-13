b93900979f3614a30267cd7765e27d308f72f704
Timothy Trindle <ttrindle@cs.washington.edu>
2016-11-13 09:42:59 -0800

    added util shell files

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+javac *.java
+scalac Util.scala
+scalac HaliteBot.scala
+scalac MyBot.scala
+scalac RandomBot.scala
--- runGame.sh
+++ runGame.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-javac *.java
-scalac HaliteBot.scala
-scalac MyBot.scala
-scalac RandomBot.scala
-
+./compile.sh
 ./halite -d "30 30" "scala MyBot" "scala RandomBot"
